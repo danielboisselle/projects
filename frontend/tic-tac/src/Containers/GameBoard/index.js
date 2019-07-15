75a6c8f46616ef94ec17a856d88e5f7c92778fd8
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2019-07-15 19:23:28 -0400

    chore: add gameBoard grids and styling

--- frontend/tic-tac/src/Containers/GameBoard/index.js
+++ frontend/tic-tac/src/Containers/GameBoard/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import styles from './GameBoard.module.css';
 
 export default function gameBoard () {
+  //TODO: create board grid components
+
+  const boardGrid = Array(9).fill(null).map((_, i) => <div key={i} className={styles.boardGridItem}></div>)
+
   return (
     <div className={styles.gameBoard}>
-
+      {boardGrid}
     </div>
   )
 }
