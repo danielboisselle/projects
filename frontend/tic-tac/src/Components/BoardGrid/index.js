de2864c127e3cd3f858fb12786356e5ae307883a
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2019-07-18 18:42:27 -0400

    chore: remove state use props

--- frontend/tic-tac/src/Components/BoardGrid/index.js
+++ frontend/tic-tac/src/Components/BoardGrid/index.js
@@ -1,36 +1,17 @@
-import React, { useContext, useState } from 'react';
-import PlayerContext from '../../Context/playerContext';
+import React from 'react';
 import styles from './BoardGrid.module.css';
 
-function BoardGrid() {
-  const context = useContext(PlayerContext);
-
-  // Player 1s turn
-  let currentTurn = 'X'
-
-  if (context.currentPlayer === 'p1') { // Player 2s turn
-    currentTurn = 'O'
-  }
-
-  // TODO: Optimize
-  const [boardGridState, setBoardGridState] = useState(Array(9).fill(null));
-
-  const boardGrid = boardGridState.map((_, i) => (
+function BoardGrid(props) {
+  const boardGrid = props.boardState.map(i => (
     <div
-      key={i}
+      key={i.key}
       className={styles.boardGridItem}
       onClick={() => {
-        if (boardGridState[i] !== null) {
-          return
-        }
+        if (i.value) return
 
-        // TODO: Optimize
-        const copy = [...boardGridState];
-        copy.splice(i, 1, currentTurn)
-        setBoardGridState(copy);
-        context.playerMoved();
+        props.playerMoved(i.key)
       }}>
-      {boardGridState[i]}
+      {i.value}
     </div>
   ))
 
