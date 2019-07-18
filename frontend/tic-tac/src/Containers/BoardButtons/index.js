6c67fcdd4b60bb2481275da6beff11a0f8200d31
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2019-07-18 19:38:25 -0400

    chore: add end game button

--- frontend/tic-tac/src/Containers/BoardButtons/index.js
+++ frontend/tic-tac/src/Containers/BoardButtons/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import styles from './BoardButtons.module.css';
 
-export default function boardButtons () {
+export default function boardButtons (props) {
+  const {
+    endGame,
+  } = props;
+
   return (
     <div className={styles.boardButtons}>
-
+      <button 
+      type="submit"
+      className={[styles.boardButtons, styles.endGame]}
+      onClick={endGame}>End game</button>
     </div>
   )
 }
