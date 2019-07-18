5abbf9edeb19413e41314e65c2e1f6c38691ff98
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2019-07-18 18:44:38 -0400

    update: add conditional gameState renders, newGame, endGame handlers

--- frontend/tic-tac/src/Components/Modal/Modal.js
+++ frontend/tic-tac/src/Components/Modal/Modal.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Modal.module.css';
 
 const Modal = props => {
-  return (
-    <div className={styles.modalBackground}>
-      <div className={styles.modal}>
-        <p>The winner is:</p>
-        <button
-          onClick={() => { console.log('clicked') }}>Ok</button>
+  const {
+    gameStatus,
+    setPlayerNames,
+    endGame,
+    newGame,
+  } = props;
+
+  const [p1Name, setP1Name] = useState('')
+  const [p2Name, setP2Name] = useState('')
+
+  if (gameStatus === 'NONE') {
+    return (
+      <div className={styles.modalBackground}>
+        <div className={styles.modal}>
+          <input onChange={e => setP1Name(e.target.value)}></input>
+          <input onChange={e => setP2Name(e.target.value)}></input>
+          <button type="submit" onClick={() => {
+            setPlayerNames(p1Name, p2Name)
+            newGame()
+          }}>Submit</button>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+  if (gameStatus === 'OVER') {
+    return (
+      <div className={styles.modalBackground}>
+        <div className={styles.modal}>
+          <p>The winner is:</p>
+          <button
+            onClick={props.newGame}>New Game</button>
+          <button
+            onClick={props.newGame}>End Game</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Modal;
