68553bc5ddee843435eabc75e06650964d78e9d4
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2019-07-23 12:15:02 -0400

    remove comments

--- backend/true-to-size-service/index.js
+++ backend/true-to-size-service/index.js
@@ -3,9 +3,6 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// const redis = require("redis");
-// const client = redis.createClient();
-
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
@@ -13,19 +10,6 @@ const port = process.env.PORT || 3000;
 const {
   TrueToSizeRouter,
 } = require('./src/routers');
-
-// const MY_KEY = 'DANSKEY';
-
-// app.get('/', function (req, res) {
-//   client.set(MY_KEY, "this is a set value and redis is working", redis.print);
-
-//   client.get(MY_KEY, (err, rep) => {
-//     console.log('responce recived', rep)
-//   })
-//   res.json({
-//     message: 'successful'
-//   })
-// })
 
 app.use('/trueToSizes', TrueToSizeRouter);
 
