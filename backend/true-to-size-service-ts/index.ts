3159af9dcab1057356bbfca676d56aade7d32dea
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2019-07-29 13:55:11 -0400

    fix: tslint

--- backend/true-to-size-service-ts/index.ts
+++ backend/true-to-size-service-ts/index.ts
@@ -1,20 +1,20 @@
-import express = require('express');
-import bodyParser = require('body-parser');
+import bodyParser = require("body-parser");
+import express = require("express");
 
 const app: express.Application = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function loggerMiddleware(request: express.Request, response: express.Response, next: Function) {
+function loggerMiddleware(request: express.Request, response: express.Response, next: () => void) {
   console.log(`${request.method} ${request.path}`);
   next();
 }
- 
+
 app.use(loggerMiddleware);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, function () {
+app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
