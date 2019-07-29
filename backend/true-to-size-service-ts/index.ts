95e09f9b59ba09222416b4b3be45e5f4988a7978
Daniel Boisselle <daniel.r.boisselle@gmail.com>
2019-07-29 12:04:23 -0400

    chore: add loggerMiddleware

--- backend/true-to-size-service-ts/index.ts
+++ backend/true-to-size-service-ts/index.ts
@@ -6,6 +6,13 @@ const app: express.Application = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+function loggerMiddleware(request: express.Request, response: express.Response, next: Function) {
+  console.log(`${request.method} ${request.path}`);
+  next();
+}
+ 
+app.use(loggerMiddleware);
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
